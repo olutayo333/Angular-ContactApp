022a0f5d3730f1d02bc38448e9a9c917b2d54b9e
Ayanrinde Olutayo Stephen <olutayostephen@gmail.com>
2024-06-27 15:49:00 +0100

    updated

--- src/app/dropdown-basic-component/dropdown-basic-component.component.ts
+++ src/app/dropdown-basic-component/dropdown-basic-component.component.ts
@@ -1,21 +1,38 @@
 import { Component } from '@angular/core';
+import { ServicesService } from '../services.service';
+import { CommonModule } from '@angular/common';
+
+interface Userobj{
+  id:number,
+  name:string,
+  email:string,
+  age:number
+}
 
 @Component({
   selector: 'app-dropdown-basic-component',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './dropdown-basic-component.component.html',
   styleUrl: './dropdown-basic-component.component.css'
 })
 export class DropdownBasicComponentComponent {
+  constructor(public serv:ServicesService){}
+  public userarray:Userobj[]=[]; 
+
+  ngOnInit(){
+    console.log(this.serv.firstName);
+    this.userarray= this.serv.getuserarray()
+   }
+
+
+
 
   openModal(){
     const modalDiv =  document.getElementById('myModal');
     if(modalDiv != null){
       modalDiv.style.display ='block';
-
     }
-    
   }
   closeModal(){
     const modalDiv =  document.getElementById('myModal');
--- src/app/services.service.ts
+++ src/app/services.service.ts
@@ -1,14 +1,49 @@
 import { Injectable } from '@angular/core';
-
+interface Userobj{
+  id:number,
+  name:string,
+  email:string,
+  age:number
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
   
   public firstName:string = "Olutayo"
+  public userarray:Userobj[]=[
+    {
+      id:1,
+      name:'Lola Johnson',
+      age:23,
+      email:'lolajohnson@gmail.com'
+    },
+    {
+      id:2,
+      name:'Olu Stephen',
+      age:25,
+      email:'olustephen@gmail.com'
+    },
+    {
+      id:3,
+      name:'olaoluwa Chris',
+      age:33,
+      email:'olachris@gmail.com'
+    },
+    {
+      id:4,
+      name:'Philips Oliutola',
+      age:43,
+      email:'philipsolutola@gmail.com'
+    }
+  ]
   
   constructor() { }
   
+  public getuserarray(){
+    return this.userarray
+  }
+
   public changefirstname(name:string){
     this.firstName="stephen"
     return this.firstName
